7258617a8a68b51b69a2d4a3795bd1b4d425fbc0
laukika-in <tech@laukika.com>
2025-06-30 15:54:41 +0530

    Update class-gmp-interest-tables.php

--- includes/class-gmp-interest-tables.php
+++ includes/class-gmp-interest-tables.php
@@ -15,8 +15,9 @@ function gmp_get_interest_data_for_subscription($subscription) {
             $data[] = [
                 'index'      => count($data) + 1,
                 'date'       => $order->get_date_created()->date('Y-m-d'),
-                'order_id'   => $order->get_order_number(),
-                'order_link' => $order->get_id(),
+                'order_id'   => $order->get_id(),
+                'order_no'   => $order->get_order_number(),
+                'order_url'  => $order->get_view_order_url(),
                 'emi'        => wc_price($emi),
                 'interest'   => wc_price($interest) . " ({$percent}%)",
                 'total'      => wc_price($emi + $interest),
@@ -26,6 +27,7 @@ function gmp_get_interest_data_for_subscription($subscription) {
 
     return $data;
 }
+
 
 function gmp_admin_related_orders_interest_table($order) {
     if (!wcs_order_contains_subscription($order)) return;
@@ -52,11 +54,11 @@ function gmp_render_interest_table($subscription, $context = 'admin') {
         echo '<tr>';
         echo "<td>{$row['index']}</td>";
         echo "<td>{$row['date']}</td>";
-       $link = ($context === 'frontend')
-    ? get_view_order_url(get_view_order_url())
-    : esc_url(admin_url("post.php?post={$row['order_link']}&action=edit"));
+        $link = ($context === 'frontend')
+    ? esc_url($row['order_url'])
+    : esc_url(admin_url("post.php?post={$row['order_id']}&action=edit"));
 
-echo "<td><a href='{$link}'>#{$row['order_id']}</a></td>";
+echo "<td><a href='{$link}'>#{$row['order_no']}</a></td>";
         echo "<td>{$row['emi']}</td>";
         echo "<td>{$row['interest']}</td>";
         echo "<td>{$row['total']}</td>";
