1edc59b9059ed6136fc3364fab31ceb596e91ce4
laukika-in <tech@laukika.com>
2025-06-30 20:52:24 +0530

    Update gold-money-plan.php

--- gold-money-plan.php
+++ gold-money-plan.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gold Money Plan
  * Description: A WooCommerce-integrated EMI saving and redemption system with interest tracking and purchase discount.
- * Version: 1.0.19
+ * Version: 1.0.20
  * Author: Your Name
  * Text Domain: gold-money-plan
  */
