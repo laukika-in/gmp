0c38d581cda3d674c1ce690f482d80143a67f33f
laukika-in <tech@laukika.com>
2025-07-03 20:26:34 +0530

    Update class-gmp-myaccount.php

--- frontend/class-gmp-myaccount.php
+++ frontend/class-gmp-myaccount.php
@@ -4,13 +4,21 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class GMP_MyAccount {
 
     public static function init() {
+        // Delay endpoint registration until rewrite system is available
+        add_action( 'init', [ __CLASS__, 'add_account_endpoint' ] );
+
+        // Account menu item
         add_filter( 'woocommerce_account_menu_items', [ __CLASS__, 'add_menu_item' ] );
+
+        // Content rendering for the endpoint
         add_action( 'woocommerce_account_gmp-cycles_endpoint', [ __CLASS__, 'render_cycle_list' ] );
 
-        add_rewrite_endpoint( 'gmp-cycles', EP_ROOT | EP_PAGES );
-
+        // Frontend assets
         add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_assets' ] );
+    }
 
+    public static function add_account_endpoint() {
+        add_rewrite_endpoint( 'gmp-cycles', EP_ROOT | EP_PAGES );
     }
 
     public static function add_menu_item( $items ) {
@@ -21,11 +29,11 @@ class GMP_MyAccount {
     public static function render_cycle_list() {
         include GMP_PLUGIN_DIR . 'views/front-cycle-list.php';
     }
-    public static function enqueue_assets() {
-    if ( is_account_page() && is_user_logged_in() && is_wc_endpoint_url( 'gmp-cycles' ) ) {
-        wp_enqueue_style( 'gmp-style', GMP_PLUGIN_URL . 'assets/css/gmp-style.css', [], GMP_PLUGIN_VERSION );
-        wp_enqueue_script( 'gmp-style', GMP_PLUGIN_URL . 'assets/js/gmp-style.js', [ 'jquery' ], GMP_PLUGIN_VERSION, true );
-    }
-}
 
+    public static function enqueue_assets() {
+        if ( is_account_page() && is_user_logged_in() && is_wc_endpoint_url( 'gmp-cycles' ) ) {
+            wp_enqueue_style( 'gmp-style', GMP_PLUGIN_URL . 'assets/css/gmp-style.css', [], GMP_PLUGIN_VERSION );
+            wp_enqueue_script( 'gmp-style', GMP_PLUGIN_URL . 'assets/js/gmp-style.js', [ 'jquery' ], GMP_PLUGIN_VERSION, true );
+        }
+    }
 }
