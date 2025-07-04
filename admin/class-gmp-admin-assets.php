2c3317f6a23ad3889667c538c999f7c49599f23f
laukika-in <tech@laukika.com>
2025-07-05 01:30:56 +0530

    Update class-gmp-admin-assets.php

--- admin/class-gmp-admin-assets.php
+++ admin/class-gmp-admin-assets.php
@@ -10,7 +10,7 @@ class GMP_Admin_Assets {
         $screen = get_current_screen();
         if ( strpos( $screen->id, 'gmp' ) !== false ) {
             wp_enqueue_style( 'gmp-admin-style', GMP_PLUGIN_URL . 'assets/css/gmp-admin.css', [], GMP_PLUGIN_VERSION );
-            wp_enqueue_script( 'gmp-admin-script', GMP_PLUGIN_URL . 'admin/js/gmp-admin.js', [ 'jquery' ], GMP_PLUGIN_VERSION, true );
+            wp_enqueue_script( 'gmp-admin-script', GMP_PLUGIN_URL . 'assets/js/gmp-admin.js', [ 'jquery' ], GMP_PLUGIN_VERSION, true );
 
             wp_localize_script( 'gmp-admin-script', 'GMP_Admin_Actions', [
                 'nonce' => wp_create_nonce( 'gmp_admin_cycle_action' )
