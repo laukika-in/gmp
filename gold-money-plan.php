39129c6c2811f26c5f00180f53fc9fe9cc09be63
laukika-in <tech@laukika.com>
2025-07-05 21:37:20 +0530

    Update gold-money-plan.php

--- gold-money-plan.php
+++ gold-money-plan.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gold Money Plan
  * Description: Custom EMI-based gold investment plugin for WooCommerce.
- * Version: 2.0.46
+ * Version: 2.0.47
  * Author: Your Name
  * Text Domain: gmp
  */
